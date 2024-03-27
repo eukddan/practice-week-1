43b16fd44cf7fff8f1c869e059c48bcbac40cf66
nongdam <126311854+nongdam@users.noreply.github.com>
2024-03-27 15:51:50 +0900

    [202014994] complete

--- submit/st202014994.c
+++ submit/st202014994.c
@@ -2,5 +2,11 @@
 
 void func(){
     printf("Hello World!");
+    for (int i = 1; i < 5; i++){
+        for (int j = 0; j < i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
